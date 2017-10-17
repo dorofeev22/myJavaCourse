e68b2e655c5d225bc3c33949eca405786db395e9
dorofeev22 <dorofeev22@yandex.ru>
2017-10-17 13:04:59 +0700

    #1 проект тестовых заданий

---
+++ src/main/java/ru/parsentev/task_001/Calculator.java
@@ -0,0 +1,38 @@
+package ru.parsentev.task_001;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author parsentev
+ * @since 18.07.2016
+ */
+public class Calculator {
+    private static final Logger log = getLogger(Calculator.class);
+
+    public double getResult() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void add(double first, double second) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void substract(double first, double second) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void multiple(double first, double second) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void div(double first, double second) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void expand(double first, double second) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/ru/parsentev/task_005/RightTriangle.java
@@ -0,0 +1,33 @@
+package ru.parsentev.task_005;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.parsentev.task_002.Point;
+import ru.parsentev.task_003.Triangle;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * TODO: comment
+ *
+ * @author parsentev
+ * @since 28.07.2016
+ */
+public class RightTriangle extends Triangle {
+    private static final Logger log = getLogger(RightTriangle.class);
+
+    public RightTriangle(Point first, Point second, Point third) {
+        super(first, second, third);
+    }
+
+    @Override
+    public boolean exists() {
+        return super.exists();
+    }
+}
