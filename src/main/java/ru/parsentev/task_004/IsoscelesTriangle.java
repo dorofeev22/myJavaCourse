a16ac0bde2fdc05ced9cee25f7190cae08bfbda9
dorofeev22 <dorofeev22@yandex.ru>
2017-11-23 15:44:35 +0700

    #5 отдельный метод сравнения double

--- src/main/java/ru/parsentev/task_004/IsoscelesTriangle.java
+++ src/main/java/ru/parsentev/task_004/IsoscelesTriangle.java
@@ -13,7 +13,9 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @since 28.07.2016
  */
 public class IsoscelesTriangle extends Triangle {
+
     private static final Logger log = getLogger(IsoscelesTriangle.class);
+    private static final double EPSILON = 0.0001;
 
     public IsoscelesTriangle(Point first, Point second, Point third) {
         super(first, second, third);
@@ -21,17 +23,29 @@ public class IsoscelesTriangle extends Triangle {
 
     @Override
     public boolean exists() {
-        boolean isExists = super.exists();
-        if (isExists) {
-            double firstSideLenght = Math.abs(first.distanceTo(second));
-            double secondSideLenght = Math.abs(second.distanceTo(third));
-            double thirdSideLenght = Math.abs(third.distanceTo(first));
-            return firstSideLenght == secondSideLenght 
-                    || secondSideLenght == thirdSideLenght 
-                    || thirdSideLenght == firstSideLenght;
-        }
-        return isExists;
+        return super.exists() && isIsosceles();
     }
 
-    
+    private boolean isIsosceles() {
+        double firstSideLenght = first.distanceTo(second);
+        double secondSideLenght = second.distanceTo(third);
+        double thirdSideLenght = third.distanceTo(first);
+        return nearlyEqual(firstSideLenght, secondSideLenght, EPSILON)
+                || nearlyEqual(secondSideLenght, thirdSideLenght, EPSILON)
+                || nearlyEqual(thirdSideLenght, firstSideLenght, EPSILON);
+    }
+
+    public static boolean nearlyEqual(double a, double b, double epsilon) {
+        final double absA = Math.abs(a);
+        final double absB = Math.abs(b);
+        final double diff = Math.abs(a - b);
+
+        if (a == b) {
+            return true;
+        } else if (a == 0 || b == 0 || diff < Float.MIN_NORMAL) {
+            return diff < (epsilon * Float.MIN_NORMAL);
+        } else {
+            return diff / Math.min((absA + absB), Float.MAX_VALUE) < epsilon;
+        }
+    }
 }
