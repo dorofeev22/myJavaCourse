1fc9c3b916c53095b65c9d754c092baaeab66395
dorofeev22 <dorofeev22@yandex.ru>
2017-11-23 21:07:56 +0700

    #8 решение задачи 7 реализовано  с использованием регулярного выражения

--- src/main/java/ru/parsentev/task_007/Expression.java
+++ src/main/java/ru/parsentev/task_007/Expression.java
@@ -1,5 +1,7 @@
 package ru.parsentev.task_007;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -20,26 +22,29 @@ public class Expression {
     }
 
     public double calc() {
-        String[] numbers = expr.split("\\+");
-        if (numbers.length == 2) {
-            return Double.valueOf(numbers[0]) + Double.valueOf(numbers[1]);
-        }
-        numbers = expr.split("-");
-        if (numbers.length == 2) {
-            return Double.valueOf(numbers[0]) - Double.valueOf(numbers[1]);
-        }
-        numbers = expr.split("/");
-        if (numbers.length == 2) {
-            return Double.valueOf(numbers[0]) / Double.valueOf(numbers[1]);
-        }
-        numbers = expr.split("\\*");
-        if (numbers.length == 2) {
-            return Double.valueOf(numbers[0]) * Double.valueOf(numbers[1]);
-        }
-        numbers = expr.split("\\^");
-        if (numbers.length == 2) {
-            return Math.pow(Double.valueOf(numbers[0]), Double.valueOf(numbers[1]));
+        Pattern pattern = Pattern.compile("[+-/*^]");
+        Matcher matcher = pattern.matcher(expr);
+        if (matcher.find()) {
+            String operator = matcher.group();
+            String[] numbers = expr.split(Pattern.quote(operator));
+            if (numbers.length == 2) {
+                double number1 = Double.valueOf(numbers[0]);
+                double number2 = Double.valueOf(numbers[1]);
+                if (operator.equals("+")) {
+                    return number1 + number2;
+                } else if (operator.equals("-")) {
+                    return number1 - number2;
+                } else if (operator.equals("/")) {
+                    return number1 / number2;
+                } else if (operator.equals("*")) {
+                    return number1 * number2;
+                } else if (operator.equals("^")) {
+                    return Math.pow(number1, number2);
+                }
+            }
+            throw new IllegalStateException(expr + " - not found two numbers");
         }
         throw new IllegalStateException(expr + " - not supported operation");
     }
+ 
 }
