69ef8f03ab94b638fef80619512e80a201ef27ad
dorofeev22 <dorofeev22@yandex.ru>
2017-11-27 16:33:27 +0700

    #17 добавлены коментарии к решению

--- src/main/java/ru/parsentev/task_016/TicTacToe.java
+++ src/main/java/ru/parsentev/task_016/TicTacToe.java
@@ -12,6 +12,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @since 28.07.2016
  */
 public class TicTacToe {
+
     private static final Logger log = getLogger(TicTacToe.class);
 
     private final int[][] values;
@@ -21,12 +22,14 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
+        // проверим наличие трех крестиков или ноликов по горизонтали
         for (int i = 0; i < values.length; i++) {
             int sumRowValues = Arrays.stream(values[i]).sum();
             if (sumRowValues == 3 || sumRowValues == 0) {
                 return true;
             }
         }
+        // приверим наличие трех крестиков или ноликов по вертикали
         for (int j = 0; j < values[0].length; j++) {
             int sumColValues = values[0][j] + values[1][j] + values[2][j];
             if (sumColValues == 3 || sumColValues == 0) {
